ad7bb8da37175ddf0efdb8301904c273af77e7e7
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2018-01-28 14:45:10 +0100

    Initial commit

---
+++ data/madrid.js
@@ -0,0 +1,118 @@
+const madridData = [
+  {
+    "name": "Bodega Alberto Ayuso",
+    "address": "Camino del Piquillo 53, Cadalso de los Vidrios.",
+    "lat": 40.3034679,
+    "lng": -4.4346626
+  },
+  {
+    "name": "Bodega Andrés Díaz",
+    "address": "Palencia, 32. Navalcarnero",
+    "lat": 40.291784,
+    "lng": -4.0100648
+  },
+  {
+    "name": "Bodega Andrés Morate",
+    "address": "Camino de Horcajuelo s/n. Belmonte del Tajo",
+    "lat": 40.1349649,
+    "lng": -3.3360592
+  },
+  {
+    "name": "Bodega Castejón",
+    "address": "Real, 118. Arganda del Rey",
+    "lat": 40.2999298,
+    "lng": -3.4362546
+  },
+  {
+    "name": "Bodega el Regajal",
+    "address": "Antigua Carretera de Andalucía, Km 50.5, 28300, Aranjuez, Madrid, España",
+    "lat": 40.0077499,
+    "lng": -3.5997352
+  },
+  {
+    "name": "Bodega Gosálbez Orti (Qubél)",
+    "address": "Real, 14. Pozuelo del Rey",
+    "lat": 40.3661895,
+    "lng": -3.3232884
+  },
+  {
+    "name": "Bodega Jesús Díaz",
+    "address": "Convento, 38, Colmenar de la Oreja, 28380, Madrid",
+    "lat": 40.107685,
+    "lng": -3.39559
+  },
+  {
+    "name": "Bodega Las Moradas de San Martín",
+    "address": "Pago Castillejos. M541, Km 4,5. San Martín de Valdeiglesias",
+    "lat": 40.3383828,
+    "lng": -4.3826182
+  },
+  {
+    "name": "Bodega Marañones",
+    "address": " Avda. Marcial Llorente, 69 - Naves B y C. Pelayos de la Presa, Madrid",
+    "lat": 40.3574031,
+    "lng": -4.3448776
+  },
+  {
+    "name": "Bodega Mariscalas",
+    "address": "Paraje las mariscalas. Poligono 13, parcela 42. Cadalso de los Vidrios",
+    "lat": 40.2818719,
+    "lng": -4.4168486
+  },
+  {
+    "name": "Bodega Muñoz Martín",
+    "address": "Calle De La Iglesia Nº4. Navalcarnero, 28600, Madrid, España",
+    "lat": 40.2884,
+    "lng": -4.014996
+  },
+  {
+    "name": "Bodega Orusco",
+    "address": "Alcalá, 48. Valdilecha",
+    "lat": 40.295808,
+    "lng": -3.301111
+  },
+  {
+    "name": "Bodega Pedro García",
+    "address": "Soledad, 10. Colmenar de Oreja",
+    "lat": 40.1076753,
+    "lng": -3.3926081
+  },
+  {
+    "name": "Bodegas Peral",
+    "address": "Bajada de las Monjas, 4. Colmenar de Oreja, 28380, Madrid",
+    "lat": 40.1228142,
+    "lng": -3.3877185
+  },
+  {
+    "name": "Bodega Real Cortijo de San Isidro",
+    "address": "Calle León Ruiz Ruiz s/n - Real Cortijo de San Isidro. Aranjuez",
+    "lat": 40.0536351,
+    "lng": -3.5665847
+  },
+  {
+    "name": "Bodega Ricardo Benito",
+    "address": "Las Eras 1. Navalcarnero",
+    "lat": 40.2888822,
+    "lng": -4.0071825
+  },
+  {
+    "name": "Bodega V de Valmores",
+    "address": "Calle del Cementerio s/n. Nuevo Baztan",
+    "lat": 40.3694568,
+    "lng": -3.2484307
+  },
+  {
+    "name": "Bodega Señorío de Val Azul",
+    "address": "Urbanización Valgrande, 37, Chinchón",
+    "lat": 40.1997877,
+    "lng": -3.4846616
+  },
+  {
+    "name": "Bodega Vinícola de Arganda",
+    "address": " Camino de San Martín de la Vega, 16, 28500, Arganda del Rey, Madrid",
+    "lat": 40.3021915,
+    "lng": -3.4598563
+  }
+];
+
+export default madridData;

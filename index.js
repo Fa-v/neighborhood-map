faa0a1c406719a9117b580b85bea248fe42e17b1
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2018-02-07 17:49:01 +0100

    filtering the list and markers

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import data from './data/madrid';
+import jsonData from './data/madrid';
 
-(function () {
+$(function () {
   'use strict';
 
   /* data that represents a single winery */
-  let Winery = function (data) {
-    this.name = ko.observable(data.name);
-    this.latitude = ko.observable(data.lat);
-    this.longitude = ko.observable(data.lng);
+  let Winery = function (winery) {
+    this.name = ko.observable(winery.name);
+    this.latitude = ko.observable(winery.lat);
+    this.longitude = ko.observable(winery.lng);
 
     this.coords = ko.computed(function () {
       return {
@@ -22,17 +22,36 @@ import data from './data/madrid';
       title: this.name(),
       animation: google.maps.Animation.DROP
     }));
-  }
+  };
 
   /* main view model */
   let viewModel = function (data) {
-    this.wineriesNames = ko.observableArray(data.map(winery => {
-      return winery.name;
-    }));
+    this.wineryNames = ko.observableArray([]);
+    this.wineries = ko.observableArray([]);
+    this.selectedWinery = ko.observable(null);
+    this.searchText = ko.observable('');
+    this.map = null;
 
-    this.wineries = ko.observableArray(data.map(wineryData => {
-      return new Winery(wineryData);
-    }));
+    for (var winery in data) {
+      this.wineryNames.push(winery.name);
+      this.wineries.push(new Winery(data[winery]));
+    }
+
+    this.showMarker = function (winery) {
+      winery.marker().setAnimation(google.maps.Animation.BOUNCE);
+      window.setTimeout(function () {
+        winery.marker().setAnimation(null);
+      }, 2000);
+      this.selectedWinery(winery);
+    }.bind(this);
+
+    this.filterWineries = ko.computed(function () {
+      let search = this.searchText().toLowerCase();
+
+      return ko.utils.arrayFilter(this.wineries(), function (winery) {
+        return winery.name().toLowerCase().indexOf(search) >= 0;
+      });
+    }.bind(this));
 
   }.bind(this);
 
@@ -41,22 +60,37 @@ import data from './data/madrid';
     init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
       /* the modelData has all the properties of the Winery class */
       const modelData = bindingContext.$data;
-
       modelData.map = new google.maps.Map(element, {
         center: { lat: 40.416775, lng: -3.703790 },
-        zoom: 10
+        zoom: 10,
+        mapTypeControl: false,
+        fullscreenControl: false
       });
     },
     update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
       const modelData = bindingContext.$data;
-      /* placing markers on the map */
-      modelData.wineries().forEach(place => {
-        place.marker().setMap(modelData.map);
-      })
+      const wineries = modelData.wineries();
+      const filteredWineries = modelData.filterWineries();
+      const filteredWineryNames = filteredWineries.map(winery => winery.name());
+      const markersToRemove = wineries.filter(winery => {
+        return filteredWineryNames.indexOf(winery.name()) === -1;
+      });
+
+      filteredWineries.forEach(winery => {
+        const marker = winery.marker()
+        marker.setMap(modelData.map);
+        marker.addListener('click', function () {
+          console.log('marker clicked!');
+        });
+      });
+
+      markersToRemove && markersToRemove.forEach(winery => {
+        winery.marker().setMap(null)
+      });
 
     }
   }
 
-  ko.applyBindings(new viewModel(data || []));
+  ko.applyBindings(new viewModel(jsonData || {}));
 
 }());
